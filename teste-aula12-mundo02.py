ac386822315a7227eb7496364539ce04830babaa
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-11 18:28:47 -0300

    Add nome ==
    Adicionei verificaçao na frente de cada or dos elif's

---
+++ teste-aula12-mundo02.py
@@ -0,0 +1,16 @@
+ #Prática teorica aula 12 - Mundo 02 
+
+#Perguntando nome ao usuário: 
+nome = str(input("Qual seu nome? "))
+
+#Se, senão se, senão
+if nome == "Maria Clara":
+    print("Seu nome é lindo!")
+elif nome == "Jovesrbardo":
+    print("Seu nome não é muito legal! Tente mudar no cartorio, {}!".format(nome))
+elif nome == "Luiz" or nome == "Marcos" or nome == "Tiago": 
+    print("Seu nome é normal, incremente algo, {}!".format(nome))
+elif nome == "Lisa" or nome == "Elisiane" or nome == "Elton" or nome == "Eunice" or nome == "Adailton":
+    print("Seu nome não é muito comum, porém, muito interessante!")
+
+print("Tenha um bom dia, {}!".format(nome))
